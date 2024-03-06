0193a2d5fa9c3e8a3c9939b56d6fbe0ae5783d35
Yuu528 <yuu528mail@gmail.com>
2024-03-06 14:14:20 +0900

    VolumeModule: Add offset control

--- src/renderer/scripts/module/VolumeModule.ts
+++ src/renderer/scripts/module/VolumeModule.ts
@@ -4,8 +4,6 @@ import ModuleCategory from '../enum/ModuleCategory'
 import JackType from '../enum/JackType'
 import Component from '../enum/Component'
 
-import ModuleData from './interface/ModuleData'
-
 import Module from './Module'
 
 export default class VolumeModule extends Module {
@@ -26,6 +24,15 @@ export default class VolumeModule extends Module {
 					max: 2,
 					step: 0.01,
 					value: 0.5
+				},
+				{
+					id: 'offset',
+					name: 'Offset',
+					component: Component.Knob,
+					min: -1,
+					max: 1,
+					step: 0.1,
+					value: 0
 				}
 			],
 			jacks: [
@@ -52,25 +59,33 @@ export default class VolumeModule extends Module {
 	onEnable(idx: number) {
 		super.onEnable(idx)
 
-		const volCtrl = this.getControl('volume')
-
-		if(volCtrl === undefined) return
-
-		const vol = volCtrl.value
+		const ctrls = this.getControls()
 
 		const input = this.moduleStore.audioCtx.createGain()
+		const offset = this.moduleStore.audioCtx.createConstantSource()
+
+		this.intNodes.offset = offset
 		this.inputs.input = input
 
-		input.gain.setValueAtTime(vol as number, this.moduleStore.audioCtx.currentTime)
+		input.gain.setValueAtTime(ctrls.volume.value as number, this.moduleStore.audioCtx.currentTime)
+		offset.offset.setValueAtTime(ctrls.offset.value as number, this.moduleStore.audioCtx.currentTime)
+		offset.connect(input)
+
+		offset.start()
 		this.outputs.output = input
 	}
 
 	updateValue(idx: number, id: string, value: number) {
 		const input = this.inputs.input as GainNode
+		const offset = this.intNodes.offset as ConstantSourceNode
 
 		switch(id) {
 			case 'volume':
 				input.gain.setValueAtTime(value, this.moduleStore.audioCtx.currentTime)
+			break
+
+			case 'offset':
+				offset.offset.setValueAtTime(value, this.moduleStore.audioCtx.currentTime)
 			break
 		}
 	}
